f7f6f194073307dab6c4bbe737ce3b22a6770136
Mohit Patil <91758557+mohitrpatil@users.noreply.github.com>
2023-12-20 16:05:32 -0700

    Time: 62 ms (73.09%), Space: 17.4 MB (25.45%) - LeetHub

--- 2706-buy-two-chocolates/2706-buy-two-chocolates.py
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.py
@@ -5,11 +5,9 @@ class Solution:
         
         count = 0
         for i in range(len(sorted_prices)):
-            if count<2 and (remaining_money - sorted_prices[i]) >=0:
+            if count<2 and (remaining_money - sorted_prices[i])>=0:
                 remaining_money = remaining_money - sorted_prices[i]
-                # print(remaining_money)
                 count += 1
-                # print("C:", count)
             else:
                 break
         
