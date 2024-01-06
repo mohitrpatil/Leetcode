3a40fde8893f775207c5f810152d8767a2adc452
Mohit Patil <91758557+mohitrpatil@users.noreply.github.com>
2024-01-05 17:44:19 -0700

    Time: 135 ms (86.47%), Space: 19.3 MB (41.41%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.py
+++ 0455-assign-cookies/0455-assign-cookies.py
@@ -1,8 +1,6 @@
 class Solution:
     def findContentChildren(self, g: List[int], s: List[int]) -> int:
-        size_pointer = 0
-        greed_pointer = 0
-        
+        size_pointer = 0        
         sorted_size_desc = sorted(s, reverse=True)
         sorted_greed_desc = sorted(g, reverse=True)
         
@@ -11,13 +9,10 @@ class Solution:
         
         count = 0
         for i in range(len(g)):
-            print("greed:", sorted_greed_desc[i])
-            print("size:", sorted_size_desc[size_pointer])
             if sorted_greed_desc[i] <= sorted_size_desc[size_pointer]:
                 count+=1
                 if len(s) == size_pointer + 1:
                     break
                 size_pointer+=1
 
-        print("count:",count)
         return count
