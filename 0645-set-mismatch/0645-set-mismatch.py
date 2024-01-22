a100c46d4784a9a434a671a679f4043891242778
Mohit Patil <91758557+mohitrpatil@users.noreply.github.com>
2024-01-21 19:01:55 -0700

    Time: 158 ms (82.84%), Space: 18.3 MB (64.05%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.py
+++ 0645-set-mismatch/0645-set-mismatch.py
@@ -1,20 +1,14 @@
 class Solution:
     def findErrorNums(self, nums: List[int]) -> List[int]:
-        count ={}
+        count = Counter(nums)
 
-        for i in nums:
-            if i in count:
-                count[i]+=1
-            else:
-                count[i]=1
-
+        keys = []
+        for key, value in count.items():
+            if value>1:
+                keys.append(key)
         
-        filt = {key: value for key, value in count.items() if value>1}
-
-        X = list(filt.keys())
-
-        for i in range(1,len(nums)+1):
+        for i in range(1, len(nums)+1):
             if i not in count:
-                X.append(i)
-
-        return X
+                keys.append(i)
+                
+        return keys
