20249852cd3ed29ef2a605cbfcd20e844838f51c
Mohit Patil <91758557+mohitrpatil@users.noreply.github.com>
2024-02-11 21:09:34 -0700

    Time: 135 ms (65.27%), Space: 18 MB (63.51%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -8,3 +8,8 @@ class Solution:
         for key, value in count.items():
             if value > thresh:
                 return key
+
+        # for i in range(len(nums)):
+        #     x = nums.count(nums[i])
+        #     if x > thresh:
+        #         return nums[i]
