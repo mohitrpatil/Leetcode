2b0068f4efcf6f4ee52d6a91511a241d4bfba829
Mohit Patil <91758557+mohitrpatil@users.noreply.github.com>
2024-02-19 18:09:29 -0700

    Time: 107 ms (83.10%), Space: 17.8 MB (64.04%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -5,18 +5,9 @@ class Solution:
         
         for i in range(len(nums)+1):
             s = s+i
-            
-        # print(s)
-        
         
         for i in range(len(nums)):
             s = s- nums[i]
             
         return s
     
-    
-#         for i in range(len(sorted_arr)):
-#             if i != sorted_arr[i]:
-#                 return i
-        
-#         return i+1
