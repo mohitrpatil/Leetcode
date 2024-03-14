32685df107587a5ef9f6e76cbc7c51480224c48d
Mohit Patil <91758557+mohitrpatil@users.noreply.github.com>
2024-03-13 19:53:21 -0600

    Time: 228 ms (34.07%), Space: 20.5 MB (28.42%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.py
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.py
@@ -1,14 +1,13 @@
 class Solution:
     def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
-        def sliding(target):
-            left_pointer = current_sum = ans = 0
+        count = {0: 1}
+        curr_sum = 0
+        total_subarrays = 0
+        
+        for num in nums:
+            curr_sum += num
+            if curr_sum - goal in count:
+                total_subarrays += count[curr_sum - goal]
+            count[curr_sum] = count.get(curr_sum, 0) + 1
 
-            for right_pointer in range(len(nums)):
-                current_sum += nums[right_pointer]
-                while current_sum > target and left_pointer <= right_pointer:
-                    current_sum -= nums[left_pointer]
-                    left_pointer += 1
-                ans += right_pointer - left_pointer + 1
-            return ans
-
-        return sliding(goal) - sliding(goal - 1)
+        return total_subarrays
