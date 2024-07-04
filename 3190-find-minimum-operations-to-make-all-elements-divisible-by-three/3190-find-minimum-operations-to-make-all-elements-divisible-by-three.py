3a715234df840bd62108f8ddb2d9126c90441b68
Mohit Patil <91758557+mohitrpatil@users.noreply.github.com>
2024-07-04 00:50:20 -0700

    Time: 71 ms (6.20%), Space: 16.4 MB (79.83%) - LeetHub

---
+++ 3190-find-minimum-operations-to-make-all-elements-divisible-by-three/3190-find-minimum-operations-to-make-all-elements-divisible-by-three.py
@@ -0,0 +1,16 @@
+class Solution:
+    def minimumOperations(self, nums: List[int]) -> int:
+        
+        x = []
+        
+        ref = [3*i for i in range(50)]
+        print(ref)
+        
+        for i in range(len(nums)):
+            mini = 9999999
+            for j in range(len(ref)):
+                mini = min(mini, abs (nums[i] - ref[j]))
+                
+            x.append(mini)
+        print(x)
+        return sum(x)
