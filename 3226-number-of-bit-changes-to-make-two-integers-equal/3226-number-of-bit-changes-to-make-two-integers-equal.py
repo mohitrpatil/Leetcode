e4278a13d5648fbb20363be2a0e1b81497682fa8
Mohit Patil <91758557+mohitrpatil@users.noreply.github.com>
2024-07-25 20:19:09 -0700

    Time: 31 ms (86.13%), Space: 16.4 MB (97.95%) - LeetHub

--- 3226-number-of-bit-changes-to-make-two-integers-equal/3226-number-of-bit-changes-to-make-two-integers-equal.py
+++ 3226-number-of-bit-changes-to-make-two-integers-equal/3226-number-of-bit-changes-to-make-two-integers-equal.py
@@ -2,10 +2,7 @@ class Solution:
     def minChanges(self, n: int, k: int) -> int:
         
         nbin = str(bin(n))[2:]
-        # print(nbin)
-        
         kbin = str(bin(k))[2:]
-        # print(kbin)
         
         if len(nbin) < len(kbin):
             while len(nbin) != len(kbin):
@@ -13,9 +10,7 @@ class Solution:
         else:
             while len(nbin) != len(kbin):
                 kbin = '0'+kbin
-                
-        print(nbin)
-        print(kbin)
+
         s = 0
         
         for i in range(len(nbin)):
